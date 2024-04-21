eca1c1d25406d18115d3bcdbfed9ac2a5976dadb
Michal Kowal <michal.kowal.66@gmail.com>
2024-04-21 18:29:57 +0200

    Setup router with index route, update module info

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"slices"
+	"strings"
 
+	"github.com/gin-gonic/gin"
+)
+
+const ServerDomain = "localhost:8080"
+
+func acceptsHTML(c *gin.Context) bool {
+	accept := c.GetHeader("Accept")
+	acceptedMimes := strings.Split(accept, ",")
+	return slices.Contains(acceptedMimes, "text/html")
+}
+
+func getIndex(c *gin.Context) {
+	if acceptsHTML(c) {
+		c.HTML(http.StatusOK, "index.tmpl", nil)
+	} else {
+		c.IndentedJSON(http.StatusNotAcceptable, nil)
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", getIndex)
+
+	router.Run(ServerDomain)
 }
